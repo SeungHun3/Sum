f198e84af9713872c465d9e072d988ea447c36e2
SeungHun <shlee4508@naver.com>
2022-05-10 11:03:30 +0900

    정렬 추가

--- Sum.cpp
+++ Sum.cpp
@@ -2,26 +2,53 @@
 
 using namespace std;
 
-void Sum(int* A, int *B)
+void Sum(const int& A, int &sum)
 {
-	*B = *B +*A;
+	sum = sum +A;
 
 }
+
 
 int main()
 {
 	int Size = 0;
 	int ASum = 0;
+
 	cin >> Size;
 
-	int A[10] = {};
+	int *A = new int[Size];
+
 	for (int i = 0; i < Size; ++i)
 	{
+		
 		cin >> A[i];
-		Sum(&A[i], &ASum);
-	}
-	cout << ASum;
+		Sum(A[i], ASum);
+		
 	
+	}
+	// 정렬함수
+	int Count = Size * Size;
+	while (Count)
+	{
+		for (int i = 0; i < Size-1; ++i)
+		{
+			if ( A[i] > A[i + 1])
+			{
+				int temp;
+				temp = A[i];
+				A[i] = A[i + 1];
+				A[i + 1] = temp;
+			}
+		}
+		Count--;
+	}
+	cout << "정렬" << endl;
+	for (int i = 0; i < Size; ++i)
+	{
+		cout << A[i] << endl;
+
+	}
+	delete[] A;
 
 	return 0;
 }
